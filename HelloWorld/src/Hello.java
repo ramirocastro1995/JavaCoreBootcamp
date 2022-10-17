81b61212df36a24ddc1270cbea07c2ca1b06ec18
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-10-17 20:24:02 -0300

    update

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello");
+
+        int myFirstNumber = (5 + 10)+(2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+    }
+}
