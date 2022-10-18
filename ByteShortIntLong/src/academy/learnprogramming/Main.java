9090a64d4f93d524196d5b77840508fd5650bbcb
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-10-18 09:56:02 -0300

    update

--- ByteShortIntLong/src/academy/learnprogramming/Main.java
+++ ByteShortIntLong/src/academy/learnprogramming/Main.java
@@ -8,5 +8,30 @@ public class Main {
         int myMaxIntValue = Integer.MAX_VALUE;
         System.out.println("Integer Minimum Value = " + myMinIntValue);
         System.out.println("Integer Max Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue+1));
+        System.out.println("Busted MIN value = " + (myMinIntValue-1));
+
+        /*int myMaxIntTest = 2147483648;*/
+
+        byte myMinByteTest = Byte.MIN_VALUE;
+        byte myMaxByteTest = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteTest);
+        System.out.println("Byte Max Value = " + myMaxByteTest);
+
+        short myMinShortTest = Short.MIN_VALUE;
+        short myMaxShortTest = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortTest);
+        System.out.println("Short Max Value = " + myMaxShortTest);
+
+        long myLongValue = 100L;
+        long myMinLongTest = Long.MIN_VALUE;
+        long myMaxLongTest = Long.MAX_VALUE;
+        System.out.println("long Minimum Value = " + myMinLongTest);
+        System.out.println("long Max Value = " + myMaxLongTest);
+        long bigLongLiteralValue = 2147483648L;
+        System.out.println(bigLongLiteralValue);
+
+        short bigShortLiteralValue = 32767;
+
     }
 }
