2c695504fc779939ca24ce728a8bae0f35847446
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-10-24 09:29:40 -0300

    update

--- overloading/src/Main.java
+++ overloading/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int newScore = calculateScore("Tim",500);
+        System.out.println("New Score is" + newScore);
+    }
+
+    public static int calculateScore(String playerName, int score) {
+        System.out.println("Player " + playerName + " scored " + score + "points");
+        return score * 1000;
     }
 }
