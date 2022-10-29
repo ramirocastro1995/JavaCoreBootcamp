eb21f9513c7fca6da9dc3770bb17e0031414edaa
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-10-29 09:37:13 -0300

    update

--- whiledo/src/Main.java
+++ whiledo/src/Main.java
@@ -1,13 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        int count = 1;
-        while(count != 6){
+        //int count = 1;
+        /*while(count != 6){
             System.out.println("Count value is " + count);
             count++;
         }
-
-        for (int i = 1; i < 7; i++){
-            System.out.println("count value is " + i);
+        for(int i=6; i!=6; i++){
+            System.out.println("count value" + count);
+        }*/
+        /*do {
+            System.out.println("count value" + count);
+            count++;
+        }while(count !=6);*/
+        /* count = 1;
+        while(true){
+            if (count == 6){
+                break;
+            }
+            System.out.println("Count value" + count);
+            count++;
+        }*/
+    }
+    public static void isEvenNumber(int num){
+        while (num % 2 != 0){
+            return true;
         }
     }
 }
