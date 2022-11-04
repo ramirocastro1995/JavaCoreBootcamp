9e5230f412d918ad371f44f74df77bbbd9945906
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-11-04 09:00:24 -0300

    update

--- Classes/src/com/rama/Main.java
+++ Classes/src/com/rama/Main.java
@@ -2,6 +2,9 @@ package com.rama;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car porsche = new Car();
+        Car holden = new Car();
+        porsche.setModel("Carrera");
+        System.out.println("Model is " + porsche.getModel());
     }
 }
