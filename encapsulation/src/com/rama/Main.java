e8f552c55da4fc14de00d5f2789389ce5ec34914
Ramiro Castro <53920544+ramirocastro1995@users.noreply.github.com>
2022-11-16 12:04:46 -0300

    update

---
+++ encapsulation/src/com/rama/Main.java
@@ -0,0 +1,22 @@
+package com.rama;
+
+public class Main {
+    public static void main(String[] args) {
+//        Player player = new Player();
+//        player.name = "Tim";
+//        //player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining healt = " + player.healthRemaining());
+//
+//        damage = 11;
+//        player.health = 200;
+//        player.loseHealth(damage);
+//        System.out.println("Remaining healt = " + player.healthRemaining());
+        EnhancedPlayer player = new EnhancedPlayer("tim",50,"Sword");
+        System.out.println("Initial health is " + player.getHealth());
+
+    }
+}
